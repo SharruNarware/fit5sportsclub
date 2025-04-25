692a53cd58e26827d359afc92a0d34e39ecd67d6
Shruti Narware <narwareshruti1402@gmail.com>
2025-04-26 00:33:16 +0530

    addin buttons

--- appcopy.js
+++ appcopy.js
@@ -4,3 +4,22 @@ const toggleBtn = document.getElementById('menu-toggle');
     toggleBtn.addEventListener('click', () => {
         mobileMenu.classList.toggle('active');
     });
+
+
+    
+    const registerBtn = document.getElementById('registerBtn');
+    const scrollUpBtn = document.getElementById('scrollUpBtn');
+
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 100) {
+        registerBtn.classList.add('show');
+        scrollUpBtn.classList.add('show');
+      } else {
+        registerBtn.classList.remove('show');
+        scrollUpBtn.classList.remove('show');
+      }
+    });
+
+    scrollUpBtn.addEventListener('click', () => {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
