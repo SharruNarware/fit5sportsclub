0562dafad3763a9a5011cbdd3fc5bc2af965f52e
Shruti Narware <narwareshruti1402@gmail.com>
2025-04-26 13:22:00 +0530

    add

--- appcopy.js
+++ appcopy.js
@@ -1,14 +1,14 @@
 const toggleBtn = document.getElementById('menu-toggle');
-    const mobileMenu = document.getElementById('mobile-menu');
+const mobileMenu = document.getElementById('mobile-menu');
     
     toggleBtn.addEventListener('click', () => {
         mobileMenu.classList.toggle('active');
     });
 
 
-    
-    const registerBtn = document.getElementById('registerBtn');
-    const scrollUpBtn = document.getElementById('scrollUpBtn');
+
+const registerBtn = document.getElementById('registerBtn');
+const scrollUpBtn = document.getElementById('scrollUpBtn');
 
     window.addEventListener('scroll', () => {
       if (window.scrollY > 100) {
@@ -23,3 +23,36 @@ const toggleBtn = document.getElementById('menu-toggle');
     scrollUpBtn.addEventListener('click', () => {
       window.scrollTo({ top: 0, behavior: 'smooth' });
     });
+
+
+
+    // Register Alert Button Code
+    document.getElementById("registerAlertBtn").addEventListener("click", function () {
+        alert("Register Now Button Clicked!");
+        });
+    
+        // Read More Alert Button Code
+        document.getElementById("readMoreAlertBtn").addEventListener("click", function () {
+        alert("Read More Button Clicked!");
+        });
+    
+    
+    
+    
+        // Sabhi Register Link Buttons pe same link lagana
+        const registerLinkButtons = document.querySelectorAll(".registerLinkBtn");
+    
+        registerLinkButtons.forEach((button) => {
+        button.addEventListener("click", function () {
+            window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSfyXeTUeA2VwTS4nYCxe9CHqcR-zhMHiJf8nPMynXGhLyW9ZA/viewform?usp=header"; // Yahan apni register page ki link do
+        });
+        });
+    
+        // Sabhi Read More Link Buttons pe same link lagana
+        const readMoreLinkButtons = document.querySelectorAll(".readMoreLinkBtn");
+    
+        readMoreLinkButtons.forEach((button) => {
+        button.addEventListener("click", function () {
+            window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSfyXeTUeA2VwTS4nYCxe9CHqcR-zhMHiJf8nPMynXGhLyW9ZA/viewform?usp=header"; // Yahan apni read more page ki link do
+        });
+        });
