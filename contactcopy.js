7b14849fe4f3e71511c027ed23588d0b3530c4cd
Shruti Narware <narwareshruti1402@gmail.com>
2025-04-27 20:17:00 +0530

    changes

--- contactcopy.js
+++ contactcopy.js
@@ -6,7 +6,6 @@ const mobileMenu = document.getElementById('mobile-menu');
     });
 
 
-
     // Page Scroll par buttons ko show/hide karna
     window.addEventListener("scroll", function() {
       const registerNowBtn = document.getElementById("registerNowBtn");
@@ -20,3 +19,22 @@ const mobileMenu = document.getElementById('mobile-menu');
           scrollUpBtn.style.display = "none";
       }
       });
+
+         // Register Now Button pe click hone par registration page open
+        document.getElementById("registerNowBtn").addEventListener("click", function() {
+            window.location.href = "https://docs.google.com/forms/d/1oFfAYtER_U4ifH5kJqQpRVVJjLLjAYFkso4u0m3TTX8/edit"; 
+        });
+            
+        // Scroll Up Button pe click hone par page top par scroll
+        document.getElementById("scrollUpBtn").addEventListener("click", function() {
+            window.scrollTo({ top: 0, behavior: "smooth" });
+        });
+        
+            
+        const registerLinkButtons = document.querySelectorAll(".registerLinkButton");
+
+        registerLinkButtons.forEach(button => {
+            button.addEventListener("click", () => {
+              window.location.href = "https://docs.google.com/forms/d/1oFfAYtER_U4ifH5kJqQpRVVJjLLjAYFkso4u0m3TTX8/edit"; 
+            });
+        });
