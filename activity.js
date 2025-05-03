1e10077677dc5139a4f14d07c6e5fdce5738f7a8
Shruti Narware <narwareshruti1402@gmail.com>
2025-05-03 18:22:31 +0530

    replace navbar

--- activity.js
+++ activity.js
@@ -1,9 +1,19 @@
-const toggleBtn = document.getElementById('menu-toggle');
-const mobileMenu = document.getElementById('mobile-menu');
-    
-    toggleBtn.addEventListener('click', () => {
-        mobileMenu.classList.toggle('active');
-    });
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+const dropdownToggles = document.querySelectorAll(".dropdown-toggle");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+// Mobile dropdown click handling
+dropdownToggles.forEach(toggle => {
+  toggle.addEventListener("click", (e) => {
+    e.preventDefault(); // prevent navigation
+    const dropdown = toggle.parentElement;
+    dropdown.classList.toggle("active");
+  });
+});
 
 
     // Page Scroll par buttons ko show/hide karna
