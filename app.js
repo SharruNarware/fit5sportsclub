0a769fcaf66d41525b08da694553fc9beb2dc720
shruti_narware <narwareshruti1402@gmail.com>
2025-06-24 01:58:11 +0530

    adding card animations

--- app.js
+++ app.js
@@ -102,3 +102,23 @@ function closeAllDropdowns() {
         window.location.href = "about.html";
       });
       });
+
+
+
+  function revealOnScroll() {
+  const elements = document.querySelectorAll(".animate-on-scroll");
+  const windowHeight = window.innerHeight;
+
+  elements.forEach((el, index) => {
+    const elementTop = el.getBoundingClientRect().top;
+
+    if (elementTop < windowHeight - 100) {
+      setTimeout(() => {
+        el.classList.add("visible");
+      }, index * 200); // 200ms delay per element
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
--- international.js
+++ international.js
@@ -105,3 +105,23 @@ dropdowns.forEach(drop => {
     drop.classList.toggle("active");
   });
 });
+
+
+
+  function revealOnScroll() {
+  const elements = document.querySelectorAll(".animate-on-scroll");
+  const windowHeight = window.innerHeight;
+
+  elements.forEach((el, index) => {
+    const elementTop = el.getBoundingClientRect().top;
+
+    if (elementTop < windowHeight - 100) {
+      setTimeout(() => {
+        el.classList.add("visible");
+      }, index * 200); // 200ms delay per element
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
